b1f2a78c62be43fb0f411a3c2689f22621554cc0
sardorReact <rovshonicon@gmail.com>
2023-03-27 17:08:08 +0500

    half of telegram has finished

---
+++ app.js
@@ -0,0 +1,18 @@
+for(let i = 0; i < 256; i++){
+    if(i % 2 == 0){
+        continue
+    }else {
+        console.log(i);
+    }
+}
+let k = 0;
+
+while(k < 256){
+    k++
+    if(k % 2 == 0){
+        continue
+    }else {
+        console.log(k);
+    }
+}
+
